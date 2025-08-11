6a9d6cfcc95b3f2c6c9b05ef3fda54cb5892f299
ganesh-786 <trump72np@gmail.com>
2025-08-11 13:24:29 +0545

    GyaanTapari is ON

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>This is me, Ganesh</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
